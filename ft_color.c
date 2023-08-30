6e79ee2d76c4caea8ccea5f7a0c8bf3b407d9f5b
reda ghouzraf <98324229+rghouzra@users.noreply.github.com>
2023-08-30 15:47:05 +0100

    Update ft_color.c to branchless code

--- ft_color.c
+++ ft_color.c
@@ -19,14 +19,8 @@ int	ft_color(t_data *data)
 	double	ratio;
 	int		color;
 
-	if (data->color == 0)
-		color = 0x00955251;
-	if (data->color == 1)
-		color = 0x00009B77;
-	if (data->color == 2)
-		color = 0x0000A170;
-	if (data->color == 3)
-		color = 0x00FFA500;
+	color = ((data->color == 0) * 0x00955251) + ((data->color == 1) * 0x00009B77) \
+		+ ((data->color == 2) * 0x0000A170) + ((data->color == 3) *0x00FFA500);
 	ft_color2(data, &color);
 	ratio = (double)(data->iteration) / (double)data->max_iteration ;
 	return (ratio * color);
